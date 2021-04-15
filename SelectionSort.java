c1c14a86537dd30cd2d34e0a0a9047628a43a47d
Patrick Puscas <ppuscas@umich.edu>
2021-04-15 10:04:04 -0400

    Fix basicSelectionSort()

--- SelectionSort.java
+++ SelectionSort.java
@@ -1,20 +1,27 @@
-
 public class SelectionSort {
 		private int temp;
-		
+		private int minIndex;
+
 	public SelectionSort() {
-	}     
-		
+	}
+
 	public int[] basicSelectionSort(int[] x) {
-		for (int i = 0; i < x.length; ++i) {
-			for (int j= i+1; j < x.length; ++j) {
-				if (x[i] > x[j]) {
-					temp = x[i];
-					x[i] = x[j];
-					temp = x[j];
-				}           
-			}
-		}
+
+		int n = x.length;
+
+        for (int i = 0; i < n-1; i++)
+        {
+            minIndex = i;
+            for (int j = i+1; j < n; j++) {
+                if (x[j] < x[minIndex]) {
+                	minIndex = j;
+                }
+            }
+
+            temp = x[minIndex];
+            x[minIndex] = x[i];
+            x[i] = temp;
+        }
 		return x;
 	}
 }
